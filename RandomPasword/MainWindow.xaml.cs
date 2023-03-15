6c50ee176e301dd566dd7b85b6cff5f07e6424bb
feyruz505 <feyruzelekberli@gmail.com>
2023-03-15 23:59:29 +0400

    Add project files.

---
+++ RandomPasword/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Bogus;
+
+namespace RandomPasword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void HandleButton_Click(object sender, RoutedEventArgs e) => PasswordLabel.Content = txtBox.Text;
+
+        private void RandomButton_Click(Object sender, RoutedEventArgs e)
+        {
+            Faker fakePasword = new Faker();
+            PasswordLabel.Content = fakePasword.Internet.Password();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
